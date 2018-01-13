dd3f2fddc7c92ed47f8fc0f099335f64da3986c6
Mkassabov <35264484+Mkassabov@users.noreply.github.com>
2018-01-13 15:24:36 -0500

    original commit

---
+++ New_web/wpscripts/global_navtree.js
@@ -0,0 +1,19 @@
+var global_navtree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Home',
+    'bIsWebPath':true,
+    'sUrl':'index.html'
+},
+{   'sTitle':'About',
+    'bIsWebPath':true,
+    'sUrl':'page2.html'
+},
+{   'sTitle':'Galleries',
+    'bIsWebPath':true,
+    'sUrl':'page3.html'
+},
+{   'sTitle':'Contact',
+    'bIsWebPath':true,
+    'sUrl':'page4.html'
+}]
+});
